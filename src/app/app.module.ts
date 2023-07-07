928f78762c9cf15811386fe8d097caea67ef31c3
Lewis J. Hannah <lewis.j.hannah@gmail.com>
2023-07-07 18:27:38 -0400

    Added single selector component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,30 @@ import { MainComponent } from './main/main.component';
 import { HeaderComponent } from './header/header.component';
 import { SideNavComponent } from './side-nav/side-nav.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SingleSelectComponent } from './reusables/single-select/single-select.component';
+import {MatSelectModule} from '@angular/material/select';
+import {ScrollingModule} from '@angular/cdk/scrolling';
+import { FormsModule } from '@angular/forms';
+import { FilterTextPipe } from './reusables/filter-text.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
     HeaderComponent,
-    SideNavComponent
+    SideNavComponent,
+    SingleSelectComponent,
+    FilterTextPipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    BrowserAnimationsModule,
+    MatSelectModule,
+    ScrollingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
