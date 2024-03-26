f76a669347507c7f636bfcb1e005f58c49a81903
Lewis J. Hannah <lewis.j.hannah@gmail.com>
2024-03-25 21:48:32 -0400

    Added search button to search page

--- src/app/search-orbs/search-orbs.component.ts
+++ src/app/search-orbs/search-orbs.component.ts
@@ -1,70 +1,72 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-search-orbs',
-  templateUrl: './search-orbs.component.html',
-  styleUrl: './search-orbs.component.scss'
+    selector: 'app-search-orbs',
+    templateUrl: './search-orbs.component.html',
+    styleUrl: './search-orbs.component.scss'
 })
 export class SearchOrbsComponent implements OnInit {
 
-  searchResult: Orb[] = [
-    {
-      location: { state: "Florida", county: "Broward"},
-      assessorUrl: "http://www.bcpa.net/search.asp",
-      taxUrl: "https://www.broward.county-taxes.com/public",
-      landUrl: "http://www.broward.org/RecordsTaxesTreasury/Records/Pages/PublicRecordsSearch.aspx"
-    },
-    {
-      location: { state: "Maryland", county: "Baltimore"},
-      assessorUrl: "http://sdat.resiusa.org/RealProperty/Pages/",
-      taxUrl: "http://www.baltimorecountymd.gov/Agencies/budfin/taxsearch/index.html",
-      landUrl: "http://v3.mdlandrec.net/main/index.cfm?CFID=37749572&CFTOKEN=96667807&jsessionid=ac30c02aebd66aef4aeb124c920121f1444eTR",
-    },
-    {
-      location: { state: "Pennsylvania", county: "Allegheny" },
-      assessorUrl: "http://www2.county.allegheny.pa.us/RealEstate/",
-      taxUrl: "Use Locality",
-      landUrl: "https://pa_allegheny.uslandrecords.com/palr/"
-    },
-    {
-      location: { state: "Pennsylvania", county: "Washington" },
-      assessorUrl: "http://washcounty.info/wcmtp/tri.asp",
-      taxUrl: "Use Locality",
-      landUrl: "http://www.landex.com/webstore/"
+    searchResult: Orb[] = [
+        {
+            location: { state: "Florida", county: "Broward" },
+            assessorUrl: "http://www.bcpa.net/search.asp",
+            taxUrl: "https://www.broward.county-taxes.com/public",
+            landUrl: "http://www.broward.org/RecordsTaxesTreasury/Records/Pages/PublicRecordsSearch.aspx"
+        },
+        {
+            location: { state: "Maryland", county: "Baltimore" },
+            assessorUrl: "http://sdat.resiusa.org/RealProperty/Pages/",
+            taxUrl: "http://www.baltimorecountymd.gov/Agencies/budfin/taxsearch/index.html",
+            landUrl: "http://v3.mdlandrec.net/main/index.cfm?CFID=37749572&CFTOKEN=96667807&jsessionid=ac30c02aebd66aef4aeb124c920121f1444eTR",
+        },
+        {
+            location: { state: "Pennsylvania", county: "Allegheny" },
+            assessorUrl: "http://www2.county.allegheny.pa.us/RealEstate/",
+            taxUrl: "Use Locality",
+            landUrl: "https://pa_allegheny.uslandrecords.com/palr/"
+        },
+        {
+            location: { state: "Pennsylvania", county: "Washington" },
+            assessorUrl: "http://washcounty.info/wcmtp/tri.asp",
+            taxUrl: "Use Locality",
+            landUrl: "http://www.landex.com/webstore/"
+        }
+    ];
+
+    stateList: string[] = [];
+    countyList: string[] = [];
+
+    ngOnInit(): void {
+        this.getStates();
     }
-  ];
 
-  stateList: string[] = [];
-  countyList: string[] = [];
+    getStates() {
+        this.stateList = [... new Set(this.searchResult.map(item => item.location.state))];
+    }
 
-  ngOnInit(): void {
-    this.getStates();
-  }
+    getCounties(selectedState: string) {
+        this.countyList = [...new Set(this.searchResult.filter((orb) => orb.location.state == selectedState).map(item => item.location.county))];
+    }
 
-  getStates() {
-    this.stateList = [... new Set(this.searchResult.map(item => item.location.state))]; 
-  }
+    onStateChange(newState: any) {
+        this.getCounties(newState);
+    }
 
-  getCounties(selectedState: string) {
-    this.countyList = [...new Set(this.searchResult.filter((orb) => orb.location.state == selectedState).map(item => item.location.county))];
-    console.log(this.countyList);
-  }
-
-  onStateChange(newState: any) {
-    console.log('State Changed! ', newState);
-    this.getCounties(newState);
-  }
+    onSearchClicked() {
+        alert("CLICKED!");
+    }
 
 }
 
 export interface Orb {
-  location: StateCounty;
-  assessorUrl: string;
-  taxUrl: string;
-  landUrl: string;
+    location: StateCounty;
+    assessorUrl: string;
+    taxUrl: string;
+    landUrl: string;
 }
 
 export interface StateCounty {
-  state: string;
-  county: string;
+    state: string;
+    county: string;
 }
